51d737ad7c6511ba3609108e6391bd0db616ddcb
Tim St. Dennis <timstdennis011@gmail.com>
2021-08-24 16:53:43 -0400

    Log In and Register Components sketched out

--- CWT/src/app/app.module.ts
+++ CWT/src/app/app.module.ts
@@ -3,18 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LogInComponentComponent } from './log-in-component/log-in-component.component';
-import { RegisterComponentComponent } from './register-component/register-component.component';
-import { RegisterUserComponent } from './register-user/register-user.component';
-import { LogInComponent } from './log-in/log-in.component';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MyComponentComponent } from './components/my-component/my-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogInComponentComponent,
-    RegisterComponentComponent,
     RegisterUserComponent,
-    LogInComponent
+    LogInComponent,
+    BannerComponent,
+    NavComponent,
+    FooterComponent,
+    MyComponentComponent
   ],
   imports: [
     BrowserModule,
