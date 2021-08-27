4b29f512a6e5bb2ca23d5a55d9f55e26cf729eb6
Tim St. Dennis <timstdennis011@gmail.com>
2021-08-27 08:51:01 -0400

    Couldn't check in due to outage

--- CWT/src/app/components/nav/nav.component.spec.ts
+++ CWT/src/app/components/nav/nav.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { NavComponent } from './nav.component';
 
 describe('NavComponent', () => {
@@ -8,6 +8,7 @@ describe('NavComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports:[RouterTestingModule],
       declarations: [ NavComponent ]
     })
     .compileComponents();
