b981b65e5d0dcfd584912cddeb0a57b9ae321380
Tim St. Dennis <timstdennis011@gmail.com>
2021-09-02 16:16:36 -0400

    Waypointing is now functional

--- CWT/src/app/components/map/map.component.ts
+++ CWT/src/app/components/map/map.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLinkWithHref } from '@angular/router';
 import { AppComponent } from 'src/app/app.component';
 import { Loader } from "@googlemaps/js-api-loader"
 import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
-import { Xliff } from '@angular/compiler';
+import { identifierModuleUrl } from '@angular/compiler';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -32,7 +32,7 @@ export class MapComponent implements OnInit {
   x: any;
   mark: any;
   single_Map: any;
-
+  WayPointsMap : Map<number, String> = new Map<number, String>();
   constructor(private router: Router, private app: AppComponent) {
     /*if(app.isTokenNull()) --Uncomment this when done with Map Features.
     {
@@ -129,7 +129,6 @@ export class MapComponent implements OnInit {
     });
   }
 
-
   ShowRoute() {
     const directionsRenderer = new google.maps.DirectionsRenderer();
     const directionsService = new google.maps.DirectionsService();
@@ -139,10 +138,23 @@ export class MapComponent implements OnInit {
       this.x = this.SetPlace(this.starting_Street, this.starting_City, this.starting_State, this.starting_ZipCode);
       this.mark = this.SetPlace(this.end_Street, this.end_City, this.end_State, this.end_ZipCode);
     }
+    this.Concatenate_WayPoints();
+    let ways : google.maps.DirectionsWaypoint[] = [];
+    if(this.WayPointsMap != null)
+    {
+      for(let i = 0; i < this.WayPointsMap.size; i++)
+      {
+        ways.push( {
+          location: this.WayPointsMap.get(i) as string,
+          stopover: false
+        });
+      }
+    }
     directionsService
       .route({
         origin: this.x,
         destination: this.mark,
+        waypoints: ways,
         travelMode: google.maps.TravelMode.DRIVING,
       }).then((response) => {
         directionsRenderer.setDirections(response);
@@ -150,7 +162,37 @@ export class MapComponent implements OnInit {
       }).catch((e) => window.alert("Directions request failed due to " + status));
   }
 
-  getMap(newInstance?: boolean) {
+  Add_Additional_Waypoint(){
+    let table = document.getElementById('repeating_WayPoints');
+    let cell = document.createElement('div');
+    let numID = 0;
+    if(table?.hasChildNodes)
+    {
+      numID = table.children.length;
+    }
+    cell.id = "WayPoint{" + numID + "}";
+    let street = document.createElement('input');
+    let city = document.createElement('input');
+    let state = document.createElement('input');
+    let zip = document.createElement('input');
+    street.innerHTML = `<input type="text" name="way_Street" id ="street${numID}">`
+    city.innerHTML = `<input type="text" name="way_City" id ="city${numID}">`
+    state.innerHTML = `<input type="text" name="way_State" id ="state${numID}">`
+    zip.innerHTML = `<input type="number" name="way_Zip" id ="zip${numID}">`
+    zip.type = "number";
+    zip.pattern = "[0-9]{5}";
+    zip.placeholder = "Zip Code";
+    cell.appendChild(street);
+    cell.appendChild(city);
+    cell.appendChild(state);
+    cell.appendChild(zip);
+    table?.appendChild(cell);
+  }
+
+  //Helper Functions
+
+  //Returns a singleton map
+  getMap(newInstance?: boolean) { 
     if (newInstance) {
       return new google.maps.Map(document.getElementById("map") as HTMLElement, {
         center: { lat: 41.7, lng: -73.7 },
@@ -169,9 +211,44 @@ export class MapComponent implements OnInit {
 
   }
 
-  SetPlace(Street: string, City: string, State: string, ZipCode: string): any {
+  //Formats the user input into something usable.
+  SetPlace(Street: string, City: string, State: string, ZipCode: string): any { 
     return Street + "," + City + "," + State + "," + ZipCode;
   }
 
-}
+  Concatenate_WayPoints()
+  {
+    let values :[any] = [""];
+    let table = document.getElementById('repeating_WayPoints') as HTMLDivElement;
 
+    table.childNodes.forEach(function (item) {
+        let ID = item as HTMLDivElement;
+        item.childNodes.forEach(function (cell) {
+          let input = cell as HTMLInputElement;
+          if(input.value != null || input.value != "")
+          {
+            values.push(ID.id + "-" + input.value);
+          }
+        })  
+    });
+    for(let i = 1; i < values.length; i++)
+    {
+      console.log(values[i]);
+      let waypoint = values[i] as string;
+      let spl = waypoint.split('-');
+      spl[0] = spl[0].slice(spl[0].indexOf('{')+1,spl[0].indexOf('}'));
+      console.log(spl[0]);
+      let WayPointNumber = parseInt(spl[0]); 
+      if(this.WayPointsMap.has(WayPointNumber))
+      {
+        let NewVal = this.WayPointsMap.get(WayPointNumber) + "," + spl[1];
+        console.log("NewVal = " + NewVal);
+        this.WayPointsMap.set(WayPointNumber, NewVal);
+      }
+      else
+      {
+        this.WayPointsMap.set(WayPointNumber, spl[1]);
+      }
+    }    
+  }
+}
