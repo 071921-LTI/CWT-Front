15401f1b2bcc74a16b796420d5eb63da22cf1539
Tim St. Dennis <timstdennis011@gmail.com>
2021-09-02 16:52:01 -0400

    Merged Code

--- CWT/src/app/components/map/map.component.ts
+++ CWT/src/app/components/map/map.component.ts
@@ -2,9 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterLinkWithHref } from '@angular/router';
 import { AppComponent } from 'src/app/app.component';
 import { Loader } from "@googlemaps/js-api-loader"
-
 import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
-import { identifierModuleUrl } from '@angular/compiler';
+import { identifierModuleUrl, Position } from '@angular/compiler';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -14,7 +13,6 @@ const httpOptions = {
   })
 };
 const locationButton = document.createElement("button");
-import { Position } from '@angular/compiler';
 
 @Component({
   selector: 'app-map',
@@ -34,7 +32,7 @@ export class MapComponent implements OnInit {
   x: any;
   mark: any;
   single_Map: any;
-  WayPointsMap : Map<number, String> = new Map<number, String>();
+  WayPointsMap: Map<number, String> = new Map<number, String>();
   constructor(private router: Router, private app: AppComponent) {
     /*if(app.isTokenNull()) --Uncomment this when done with Map Features.
     {
@@ -46,7 +44,6 @@ export class MapComponent implements OnInit {
     this.addMapsScript();
     //}
   }
-
 
   ngOnInit(): void {
   }
@@ -70,6 +67,7 @@ export class MapComponent implements OnInit {
       this.execute_Map();
     }
   }
+
   execute_Map(): void {
     //Marker Listener
     this.getMap().addListener
@@ -129,76 +127,6 @@ export class MapComponent implements OnInit {
       });
       this.mark = mapsMouseEvent.latLng;
     });
-    
-    // Current Location Function
-    let infoWindow = new google.maps.InfoWindow();
-
-    const locationButton = document.createElement("button");
-    locationButton.textContent = "Pin to Current Location";
-    locationButton.classList.add("custom-map-control-button");
-
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-    locationButton.addEventListener("click", () => {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position: GeolocationPosition) => {
-              const pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude,
-            };
-
-            infoWindow.setPosition(pos);
-            map.setCenter(pos);
-            const marker = new google.maps.Marker({
-              position: pos,
-              map,
-            });
-            this.x = pos;
-          }
-        );
-      } 
-    });
-    //Marker Function
-
-    map.addListener("click", (mapsMouseEvent:any) => {
-      const marker = new google.maps.Marker({
-        position: mapsMouseEvent.latLng,
-        map,
-      });
-      this.mark = mapsMouseEvent.latLng;
-    });
-
-
-    // Direction Function
-    const directionsRenderer = new google.maps.DirectionsRenderer();
-    const directionsService = new google.maps.DirectionsService();
-    directionsRenderer.setMap(map);
-    directionsRenderer.setPanel(document.getElementById("sidebar") as HTMLElement);
-    const control = document.getElementById("floating-panel") as HTMLElement;
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
-    const onChangeHandler =  () => {
-      // const start = (document.getElementById("start") as HTMLInputElement).value;
-      // let start:any = "21 Elton Court,Uncasville,CT";
-      // let end:any = "30 norwitch road,CT";
-      // const end = (document.getElementById("end") as HTMLInputElement).value;
-
-      directionsService
-      .route({
-        origin: this.x,
-        destination: this.mark,
-        travelMode: google.maps.TravelMode.DRIVING,
-      })
-      .then((response) => {
-        directionsRenderer.setDirections(response);
-        console.log(response);
-      })
-      .catch((e) => window.alert("Directions request failed due to " + status));
-
-    };
-
-    (document.getElementById("GO") as HTMLElement).addEventListener(
-      "click",
-      onChangeHandler
-    );
   }
 
   ShowRoute() {
@@ -211,12 +139,10 @@ export class MapComponent implements OnInit {
       this.mark = this.SetPlace(this.end_Street, this.end_City, this.end_State, this.end_ZipCode);
     }
     this.Concatenate_WayPoints();
-    let ways : google.maps.DirectionsWaypoint[] = [];
-    if(this.WayPointsMap != null)
-    {
-      for(let i = 0; i < this.WayPointsMap.size; i++)
-      {
-        ways.push( {
+    let ways: google.maps.DirectionsWaypoint[] = [];
+    if (this.WayPointsMap != null) {
+      for (let i = 0; i < this.WayPointsMap.size; i++) {
+        ways.push({
           location: this.WayPointsMap.get(i) as string,
           stopover: false
         });
@@ -234,12 +160,11 @@ export class MapComponent implements OnInit {
       }).catch((e) => window.alert("Directions request failed due to " + status));
   }
 
-  Add_Additional_Waypoint(){
+  Add_Additional_Waypoint() {
     let table = document.getElementById('repeating_WayPoints');
     let cell = document.createElement('div');
     let numID = 0;
-    if(table?.hasChildNodes)
-    {
+    if (table?.hasChildNodes) {
       numID = table.children.length;
     }
     cell.id = "WayPoint{" + numID + "}";
@@ -264,7 +189,7 @@ export class MapComponent implements OnInit {
   //Helper Functions
 
   //Returns a singleton map
-  getMap(newInstance?: boolean) { 
+  getMap(newInstance?: boolean) {
     if (newInstance) {
       return new google.maps.Map(document.getElementById("map") as HTMLElement, {
         center: { lat: 41.7, lng: -73.7 },
@@ -284,43 +209,38 @@ export class MapComponent implements OnInit {
   }
 
   //Formats the user input into something usable.
-  SetPlace(Street: string, City: string, State: string, ZipCode: string): any { 
+  SetPlace(Street: string, City: string, State: string, ZipCode: string): any {
     return Street + "," + City + "," + State + "," + ZipCode;
   }
 
-  Concatenate_WayPoints()
-  {
-    let values :[any] = [""];
+  Concatenate_WayPoints() {
+    let values: [any] = [""];
     let table = document.getElementById('repeating_WayPoints') as HTMLDivElement;
 
     table.childNodes.forEach(function (item) {
-        let ID = item as HTMLDivElement;
-        item.childNodes.forEach(function (cell) {
-          let input = cell as HTMLInputElement;
-          if(input.value != null || input.value != "")
-          {
-            values.push(ID.id + "-" + input.value);
-          }
-        })  
+      let ID = item as HTMLDivElement;
+      item.childNodes.forEach(function (cell) {
+        let input = cell as HTMLInputElement;
+        if (input.value != null || input.value != "") {
+          values.push(ID.id + "-" + input.value);
+        }
+      })
     });
-    for(let i = 1; i < values.length; i++)
-    {
+    for (let i = 1; i < values.length; i++) {
       console.log(values[i]);
       let waypoint = values[i] as string;
       let spl = waypoint.split('-');
-      spl[0] = spl[0].slice(spl[0].indexOf('{')+1,spl[0].indexOf('}'));
+      spl[0] = spl[0].slice(spl[0].indexOf('{') + 1, spl[0].indexOf('}'));
       console.log(spl[0]);
-      let WayPointNumber = parseInt(spl[0]); 
-      if(this.WayPointsMap.has(WayPointNumber))
-      {
+      let WayPointNumber = parseInt(spl[0]);
+      if (this.WayPointsMap.has(WayPointNumber)) {
         let NewVal = this.WayPointsMap.get(WayPointNumber) + "," + spl[1];
         console.log("NewVal = " + NewVal);
         this.WayPointsMap.set(WayPointNumber, NewVal);
       }
-      else
-      {
+      else {
         this.WayPointsMap.set(WayPointNumber, spl[1]);
       }
-    }    
+    }
   }
 }
